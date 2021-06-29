df9e66f8412269eb464a8cef367c7b4e6518cc0c
Austin Huffman <austinhuffman0@gmail.com>
2021-06-28 23:46:28 -0400

    Search Functionality Complete. More Comments Added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 var getWeatherBtn = $('.weatherBtn');
 var submitBtn = $('.submit');
-console.log(submitBtn.val());
+
 var APIKey = "acf5cae1c94cfb7dfd80656854710bd9";
 
 function getWeatherAPI(city){
@@ -26,9 +26,7 @@ function getWeatherAPI(city){
         
         lat = data.coord.lat;
         lon = data.coord.lon;
-        console.log(lat);
 
-        console.log(lat);
         var emojiCode= weatherObject.Weather;
         var emojiUrl = "https://openweathermap.org/img/w/"+ emojiCode + ".png";
         $('#wEmoji').attr('src',emojiUrl);
@@ -63,19 +61,21 @@ function getWeatherAPI(city){
         return response.json();      
     })
 
+    //Third call to get 5 Day forcast using onecall rather than the standard q? query 
     .then (function (f){
-        var forecast = f.daily;
-        console.log(forecast);
-        for(var i=0; i < 5;i++)
+
+        var forecast = f.daily; //Helps with readibility
+
+        for(var i=0; i < 5;i++) //loops through all 5 days, both buttons and api days
         {   
-            weatherCard = $(".w-card-"+(i+1));
-            weatherCard.children(".Temp").text("Temperature: " + Math.trunc(forecast[i].temp.day) + "°");
+            weatherCard = $(".w-card-"+(i+1)); //Appending a number to act as a recognizable id in the loop 
+            weatherCard.children(".Temp").text("Temperature: " + Math.trunc(forecast[i].temp.day) + "°"); //Truncate is used to get rid of the decimals
             weatherCard.children(".Wind").text("Wind Speed: " + forecast[i].wind_speed + " mph");
             weatherCard.children(".Humidity").text("Humidity: " + forecast[i].humidity + "%");
 
+            //Same as Above for converting the icon to a usable image
             var emojiCode= forecast[i].weather[0].icon;
             var emojiUrl = "https://openweathermap.org/img/w/"+ emojiCode + ".png";
-            
             weatherCard.children().children().attr('src',emojiUrl);
     
         }
@@ -88,5 +88,9 @@ getWeatherBtn.click(function(){
     getWeatherAPI($(this).text()); //Passes the name of the button (City Name)
 });
 
+submitBtn.click(function(){
+    console.log("I work");
+     getWeatherAPI($(".search").val())
+})
 
 
